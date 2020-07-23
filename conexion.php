477eb3173f7128ebd3ea8e8d64ef31f7357a2485
piramidecustomers <67619608+piramidecustomers@users.noreply.github.com>
2020-07-23 13:17:43 -0500

    Delete conexion.php

--- conexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $server="bleamcorspmyl7wd2a35-mysql.services.clever-cloud.com";
-    $user="uuq9udaawurdnlyo";
-    $pass="vqWXOIK2P1U1yhdmMOu9";
-    $bd="bleamcorspmyl7wd2a35";
-
-    $conexion = mysqli_connect($server, $user, $pass, $bd) or die ("Error en la conexion");
-?>
