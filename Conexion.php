90d1220616e7abe79c006e00ba3f96009187526d
piramidecustomers <67619608+piramidecustomers@users.noreply.github.com>
2020-07-23 13:18:21 -0500

    Create Conexion.php

---
+++ Conexion.php
@@ -0,0 +1,8 @@
+<?php
+    $server="bleamcorspmyl7wd2a35-mysql.services.clever-cloud.com";
+    $user="uuq9udaawurdnlyo";
+    $pass="vqWXOIK2P1U1yhdmMOu9";
+    $bd="bleamcorspmyl7wd2a35";
+
+    $conexion = mysqli_connect($server, $user, $pass, $bd) or die ("Error en la conexion");
+?>
